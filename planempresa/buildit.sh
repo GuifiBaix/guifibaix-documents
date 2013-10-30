5b64c39b30a87971cf2ed02c30747e97ee20b311
David García Garzón <voki@canvoki.net>
2013-10-30 14:00:54 +0100

    Usa pdftk y no pdfjoin para añadir la portada

--- planempresa/buildit.sh
+++ planempresa/buildit.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-# sudo apt-get install pandoc texlive-lang-spanish
+# sudo apt-get install pandoc texlive-lang-spanish pdftk
 
 pandoc \
 	-V lang=spanish \
 	-V title="Plan de Empresa: GuifiBaix SCCL" \
 	-V documentclass=book \
-	-N --toc -f markdown *.md --template default.latex -o planempresa.pdf
+	-V papersize=a4paper \
+	-N --toc -f markdown *.md \
+	--template default.latex -o planempresa-content.pdf
+
+pdftk portada.pdf planempresa-content.pdf cat output planempresa.pdf
