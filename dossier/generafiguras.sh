5608785ecf89efce29b1fd62a7f37b2fa97b7924
David García Garzón <voki@canvoki.net>
2013-12-08 12:01:17 +0100

    adaptado generafiguras a la nueva localizacion

--- dossier/generafiguras.sh
+++ dossier/generafiguras.sh
@@ -2,9 +2,9 @@
 # sudo apt-get install inkscape
 
 for a in \
-	../../../web/content/media/images/figura-xarxaciutadana-cami.svg \
-	../../../web/content/media/images/figura-telefoniaip.svg \
-	../../logos/logo-guifibaix.svg \
+	../../web/content/media/images/figura-xarxaciutadana-cami.svg \
+	../../web/content/media/images/figura-telefoniaip.svg \
+	../logos/logo-guifibaix.svg \
 	; do
 	echo '**' Procesando $(basename $a .svg)...
 	inkscape $a -A $(basename $a .svg).pdf
